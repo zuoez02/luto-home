c8f459b032d73c8f0672ed32bea7e8c8cc880084
Luto Yvan <zuoez02@outlook.com>
2015-10-20 00:03:50 +0800

    add some comments in app.js\n add a middleware in routes/users

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', function(req, res, next) {
+  if(req.params.id == 0) {
+  	console.log(req.params.id);
+  	next();
+  } else {
+  	next('route');
+  }
+}, function (req,res) {
+	res.send('end1');
+});
+
+router.get('/:id', function(req, res) {
+	res.send('end2');
 });
 
 module.exports = router;
